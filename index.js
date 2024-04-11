76916a0f4ec58f3dc84da913217d9e50108b7671
Sergio Castro <sergiojcr16@gmail.com>
2024-04-11 17:05:11 -0600

    adding

--- index.js
+++ index.js
@@ -1,79 +1,69 @@
 const app = require("express")();
-const {spawn} = require('child_process');
-let {PythonShell} = require('python-shell')
-const qrCodeReader = false;
+const { spawn } = require('child_process');
+const { PythonShell } = require('python-shell');
 
+const qrCodeReader = true;
 
-function runPy(){
-    return new Promise(async function(resolve, reject){
-          let options = {
-          mode: 'text',
-          pythonOptions: ['-u'],
-          scriptPath: './',//Path to your script
-         };
+function runPy() {
+    return new Promise(async function (resolve, reject) {
+        let options = {
+            mode: 'text',
+            pythonOptions: ['-u'],
+            scriptPath: './', // Path to your script
+        };
 
-          await PythonShell.run('scanner.py', options, function (err, results) {
-          //On 'results' we get list of strings of all print done in your py scripts sequentially. 
-          if (err) throw err;
-          console.log('results: ');
-          for(let i of results){
+        await PythonShell.run('scanner.py', options, function (err, results) {
+            if (err) throw err;
+            console.log('results: ');
+            for (let i of results) {
                 console.log(i, "---->", typeof i)
-          }
-      resolve(results[1])//I returned only JSON(Stringified) out of all string I got from py script
-     });
-   })
- } 
-
-function runMainQrCode(){
-    return new Promise(async function(resolve, reject){
-        let r =  await runPy()
-        console.log(JSON.parse(JSON.stringify(r.toString())), "Done...!@")//Approach to parse string to JSON.
+            }
+            resolve(results[1])
+        });
     })
- }
+}
 
- if(qrCodeReader==true){
-  runMainQrCode()
- }
+function runMainQrCode() {
+    return new Promise(async function (resolve, reject) {
+        let r = await runPy()
+        console.log(JSON.parse(JSON.stringify(r.toString())), "Done...!@")
+    })
+}
 
-function runServerPy(){
-    return new Promise(async function(resolve, reject){
-          let options = {
-          mode: 'text',
-          pythonOptions: ['-u'],
-          scriptPath: './',//Path to your script
-         };
+async function runServerPy() {
+    let r = await new Promise(function (resolve, reject) {
+        let options = {
+            mode: 'text',
+            pythonOptions: ['-u'],
+            scriptPath: './', // Path to your script
+        };
 
-          await PythonShell.run('serverGPIO.py', options, function (err, results) {
-          //On 'results' we get list of strings of all print done in your py scripts sequentially. 
-          if (err) throw err;
-          console.log('results: ');
-          for(let i of results){
+        PythonShell.run('serverGPIO.py', options, function (err, results) {
+            if (err) throw err;
+            console.log('results: ');
+            for (let i of results) {
                 console.log(i, "---->", typeof i)
-          }
-      resolve(results[1])//I returned only JSON(Stringified) out of all string I got from py script
-     });
-   })
- } 
-
-function runMainServerCode(){
-    return new Promise(async function(resolve, reject){
-        let r =  await runServerPy()
-        console.log(JSON.parse(JSON.stringify(r.toString())), "Done...!@")//Approach to parse string to JSON.
-    })
- }
-
+            }
+            resolve(results[1])
+        });
+    });
+    console.log(JSON.parse(JSON.stringify(r.toString())), "Done...!@");
+}
 
 const PORT = 7777;
 
-app.listen(PORT, () => console.log(`its alive on https://localhost:${PORT}`));
-
-
-app.get('/',function(req,res) {
-  var dataToSend;
- // spawn new child process to call the python script
- const python = spawn('python', [__dirname + '/python_code.py']);
-  res.sendFile(__dirname + '/index.html');
+app.listen(PORT, () => {
+    console.log(`its alive on https://localhost:${PORT}`);
+    
+    if (qrCodeReader) {
+        runMainQrCode();
+    }
+    runServerPy();
 });
 
-
-runServerPy()
+app.get('/', function (req, res) {
+    var dataToSend;
+    // spawn new child process to call the python script
+    const python = spawn('python', [__dirname + '/python_code.py']);
+    res.sendFile(__dirname + '/index.html');
+});
